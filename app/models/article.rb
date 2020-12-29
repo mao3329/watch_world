a20e98e246896630a2b8cdf027c7ed4836581ea6
mao3329 <74639010+mao3329@users.noreply.github.com>
2020-12-29 11:55:55 +0900

    commentsモデル、テーブルの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presences: true do
+  with_options presence: true do
     validates :title
     validates :text
     validates :image
