e4095d581a669b967121ed75455af5982cfbfbbe
mao3329 <74639010+mao3329@users.noreply.github.com>
2020-12-29 20:13:08 +0900

    カテゴリidカラムの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :article_tags
+  has_many :comments, dependent: :destroy
+  has_many :article_tags, dependent: :destroy
   has_many :tags, through: :article_tags
   has_one_attached :image
 
