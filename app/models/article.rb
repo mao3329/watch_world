b605714c924e5e8d1782817ad30e9648163c6e51
mao3329 <74639010+mao3329@users.noreply.github.com>
2020-12-29 19:23:58 +0900

    ビューモデルの編集

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments
-  has_many :tags, through: :article_tags
   has_many :article_tags
+  has_many :tags, through: :article_tags
   has_one_attached :image
 
 end
