77e2d466e54d04beecb7886bee93c93485bbc22d
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-01-04 15:44:34 +0900

    コントローラーの設定

--- app/controllers/favorite_articles_controller.rb
+++ app/controllers/favorite_articles_controller.rb
@@ -1,2 +1,20 @@
 class FavoriteArticlesController < ApplicationController
+  before_action :set_article
+  before_action :authenticate_user!
+
+  def create
+    if @article.user_id != current_user.id
+      @favorite_article = FavoriteArticle.create(user_id: current_user.id, article_id: @article.id)
+    end
+  end
+
+  def destroy
+    @favorite_article = FavoriteArticle.find_by(user_id: current_user.id, article_id: @article.id)
+    @favorite_article.destroy
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
