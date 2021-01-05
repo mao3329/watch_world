d2067fd0390e19eba61729987c16fc7021282700
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-01-05 11:01:09 +0900

    モデルのアソシエーションの記述、アクションの設定

--- db/migrate/20210104094436_create_relationships.rb
+++ db/migrate/20210104094436_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
       t.references :user,   foreign_key: true
-      t.references :follow, foreign_kye: { to_table: :uers }
+      t.references :follow, foreign_key: { to_table: :uers }
 
       t.timestamps
 
