3037ac6e0d59bcb94137699730469714db0390d5
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-01-11 22:14:42 +0900

    コントローラーの設定

--- app/controllers/admin/managements_controller.rb
+++ app/controllers/admin/managements_controller.rb
@@ -1,3 +1,14 @@
 class Admin::ManagementsController < ApplicationController
-  
+  before_action :not_admin
+  before_action :set_article
+
+  private
+
+  def not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
