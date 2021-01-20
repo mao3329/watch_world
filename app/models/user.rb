ff1468eeffe38742065536ad797b34d9c89017f0
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-01-20 16:13:08 +0900

    useモデルのnicknameの一意性の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: {case_sensitive: true}
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
