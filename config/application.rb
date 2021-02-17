318b401af3eaaf6d5815c78136e036b17da8c4f1
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-17 17:36:54 +0900

    Add White List

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.hosts << "www.watchworld0730.tk"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
