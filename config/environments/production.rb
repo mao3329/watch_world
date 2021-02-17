e7463141e6c9f96d04bcd5d32b427f66ccce7dc2
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-17 17:33:06 +0900

    Add White List

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << "www.watchworld0730.tk"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
