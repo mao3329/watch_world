d702ff0ffeb6d6068f4df59125d3e9513ff8b288
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-19 09:38:33 +0900

    Hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorite_articles, dependent: :destroy
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -14,20 +10,5 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-
-  def follow(other_user)
-    unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
-    end
-  end
-
-  def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user.id)
-    relationship.destroy if relationship
-  end
-
-  def following?(other_user)
-    self.followings.include?(other_user)
-  end
   
 end
